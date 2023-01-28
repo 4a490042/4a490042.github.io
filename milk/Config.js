5fe530c03fbffa2a994a2fdc5a1d3f4a2a815e3f
4a490042 <44599058+4a490042@users.noreply.github.com>
2023-01-28 13:21:30 +0800

    Add files via upload

---
+++ milk/Config.js
@@ -0,0 +1,12 @@
+// 
+var Source = "MMSWeb";
+
+var SingleEventUrl="ws://127.0.0.1:81";
+// var SingleEventUrl="ws://192.168.1.114:82";
+var SingleEventModule = "WebSingleEvent";
+
+var MMSTarget = "MMS";
+
+var PHPModule = "PHP";
+// var PHPUrl = "http://127.0.0.1/MMS_J/KAPI/index.php/";
+var PHPUrl = "http://192.168.1.116/MMS_J/KAPI/index.php/";
---
+++ milk/Config.ts
@@ -0,0 +1,9 @@
+var Source="MMSWeb";
+//var SingleEventUrl= "ws://211.20.170.217:82";
+var SingleEventUrl= "ws://127.0.0.1:81";
+var SingleEventModule="WebSingleEvent";
+var MMSTarget="MMS";
+
+var PHPModule="PHP";
+//var PHPUrl= "http://211.20.170.217:8080/";
+var PHPUrl= "http://192.168.1.132/";
